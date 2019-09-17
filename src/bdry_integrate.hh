64f9785f91c15c6c4f3bbf9ca8dcf4f86c918b90
AnaRadic <44526598+AnaRadic@users.noreply.github.com>
2019-09-17 17:22:01 +0200

    const dim 2 and pressure 0.0 - test

--- src/bdry_integrate.hh
+++ src/bdry_integrate.hh
@@ -16,47 +16,55 @@
 #include <dune/common/parallel/mpihelper.hh>
 #include <dune/common/fvector.hh>
 #include <dune/geometry/quadraturerules.hh>
+#include <dune/grid/albertagrid/dgfparser.hh>
 #include "driver.hh"
+
+#include <dune/common/fvector.hh>
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/geometry/quadraturerules.hh>
+#include <dune/grid/yaspgrid.hh>
+
+template <int dim>
+using Point = Dune::FieldVector<double, dim>;
 
 
 template <typename GV, typename PARAMS>
-double bdry_integration(const GV& gv, double dt, double tfin,PARAMS & parameters)
+double bdry_integration(const GV &gv, double dt, double tfin,PARAMS & parameters)
 
 {
 
   double time = 0.0;
+  const int dim = 2;
   double integral = 0.0;
+  
   // Petlja po svim elementima
-    while (time < tfin-1e-8) {
+  while (time <= tfin-1e-8) {
+     
+     for (auto const &element : elements(gv)) {
+    
+        double elem_integral = 0.0;
+        // petlja po svim stranicama elementa
+       for (auto const &side : intersections(gv, element)) {
+     //  auto xg = intersections.geometry().global( coord ); //dohvaćamo koordinate
 
-  {
-
-      for (auto const &element : elements(gv))
+       if (side.boundary()/*&& xg[0]>0.0 )*/) // Jesmo li na granici domene?
        {
-    double elem_integral = 0.0;
-    // petlja po svim stranicama elementa
-    for (auto const &side : intersections(gv, element)) {
-    auto xg = intersection.geometry().global( coord );
-
-      if (side.boundary()&& xg[0]>0.0 )) // Jesmo li na granici domene?
-      {
-
-
         const auto sidegeo = side.geometry();
         auto outerNormal = side.centerUnitOuterNormal();
-        // ZatraĹľimo kvadraturnu formulu na stranici (dimenzija dim -1)
+        // Zatrazimo kvadraturnu formulu na stranici (dimenzija dim -1)
         const auto &rule =
-            Dune::QuadratureRules<double, dim - 1>::rule(sidegeo.type(), p);
-        if (rule.order() < p)
-             std::cerr << "Integracijska formula reda " << p << " nije dostupna.\n";
+            Dune::QuadratureRules<double, dim - 1>::rule(sidegeo.type(), tfin);
+        if (rule.order() < tfin)
+             std::cerr << "Integracijska formula reda " << tfin << " nije dostupna.\n";
         double result = 0.0;
         // Petlja po svim integracijskim toÄŤkama
         for (auto const &qpoint : rule) {
           double weight = qpoint.weight();
           // | det (grad g) | daje Geometry objekt
           double detjac = sidegeo.integrationElement(qpoint.position());
+          Point<dim> pressure (0.0);
           result += (pressure * outerNormal) * weight * detjac;
-        }
+       }
 
         elem_integral += result;
       }
@@ -69,4 +77,5 @@ double bdry_integration(const GV& gv, double dt, double tfin,PARAMS & parameters
   return integral;
 
 }
+
 
